267b15c2cb270b088325883211be75e3517453f7
Ian Christiansen <ianc.songdog@gmail.com>
2022-02-23 16:17:10 -0800

    created usercontext

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,12 +1,16 @@
 import './AuthForm.css';
 import { useState } from 'react';
+import { signUpUser } from '../../services/users';
 
-export default function AuthForm() {
+export default function AuthForm({ isSigningIn = true }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="email" />
       <input type="password" />
       <button type="submit">Submit</button>
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,5 +1,26 @@
-// UserProvider
-// get current user
-// setuser
+import { createContext, useContext, useState } from 'react';
+import { getUser } from '../services/users';
 
-//useUser
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(
+    currentUser ? { id: currentUser.id, email: currentUser.email } : {}
+  );
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserContext provider.');
+  }
+  return context;
+};
